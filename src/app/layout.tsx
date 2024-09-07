940e49d957e3588ec0a0dcb4ec006164435d3014
Manan <manansonani7979@gmail.com>
2024-09-07 16:11:05 +0530

    Added webhooks/route.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider, ClerkLoading, ClerkLoaded } from '@clerk/nextjs';
 import './globals.css';
 import Header from '@/components/Header';
+import Loader from '@/components/Loader';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,8 +21,13 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body className={inter.className}>
-					<Header />
-					{children}
+					<ClerkLoading>
+						<Loader />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<Header />
+						{children}
+					</ClerkLoaded>
 				</body>
 			</html>
 		</ClerkProvider>
